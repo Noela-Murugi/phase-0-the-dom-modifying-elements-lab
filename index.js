3d58e82639983e6076da0b7b3171ccc4b1ca12ab
Noela-Murugi <noela.murugi@student.moringaschool.com>
2022-08-10 16:25:12 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+
+const element = document.getElementById("main")
+element.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Noela is the champion"
+body.append(newHeader);
